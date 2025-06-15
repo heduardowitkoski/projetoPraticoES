e9b96e5ea861855891fef961c75e4394f816c6bf
heduardowitkoski <135649421+heduardowitkoski@users.noreply.github.com>
2025-06-15 19:45:19 -0300

    Create Enade.java

---
+++ Enade.java
@@ -0,0 +1,18 @@
+public class Enade implements RequisitoIntegralizacao {
+    private boolean concluido = false;
+
+    @Override
+    public String getNome() {
+        return "Situação regular no ENADE";
+    }
+
+    @Override
+    public boolean isConcluido() {
+        return concluido;
+    }
+
+    @Override
+    public void concluir() {
+        concluido = true;
+    }
+}
