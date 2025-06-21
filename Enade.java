bf4c6ecaac62c56213b65cb23e6095b6912c9932
heduardowitkoski <135649421+heduardowitkoski@users.noreply.github.com>
2025-06-21 20:09:20 -0300

    Update Enade.java

--- Enade.java
+++ Enade.java
@@ -15,4 +15,9 @@ public class Enade implements RequisitoIntegralizacao {
     public void concluir() {
         concluido = true;
     }
+
+    @Override
+    public String toString() {
+        return getNome() + (concluido ? " ✅" : " ❌");
+    }
 }
